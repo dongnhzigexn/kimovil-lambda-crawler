dd47e73734262f55fefcbd74160064400a0308f8
NgoHungDong <dongngo0804@gmail.com>
2019-01-06 17:22:22 +0700

    Change crawl logic

--- kimovil-list-handler/lambda_function.rb
+++ kimovil-list-handler/lambda_function.rb
@@ -18,23 +18,23 @@ def lambda_handler(event:, context:)
   doc = Nokogiri::HTML(res['content'])
 
   next_page_link = 'https://www.kimovil.com' + res['next_page_url'] + '?xhr=1'
-  
   phone_detail_links = doc.css('div.item-wrapper a.open-newtab').map { |a| { url: a['href'] } }
-  
+
+  threads = []
+  threads << Thread.new { LambdaClient.invoke('kimovil-list-handler-2', { url: next_page_link }) }
   phone_detail_links.each do |link|
-    # Invoke lambda handler for detail link
-    LambdaClient.invoke('kimovil-detail-handler-2', link)
+    threads << Thread.new do
+      # Invoke lambda handler for detail link
+      LambdaClient.invoke('kimovil-detail-handler-2', link)
+    end
   end
-  
-  # Invoke lambda handler for next page
-  LambdaClient.invoke('kimovil-list-handler-2', { url: next_page_link })
+  threads.map(&:join)
 
   # Save crawled page to db
-  DynamoDb.save_to_db('kimovil-crawled-pages', { url: url })
+  DynamoDb.save_to_db('kimovil-crawled-pages', { url: url, created_at: Time.now.to_s })
 
 rescue => e
   puts "ERROR: #{e}"
 end
 
-
-lambda_handler(event: { 'url' => 'https://www.kimovil.com/en/compare-smartphones/page.45?xhr=1'}, context: nil)
+# lambda_handler(event: { 'url' => 'https://www.kimovil.com/en/compare-smartphones/page.49?xhr=1'}, context: nil)
